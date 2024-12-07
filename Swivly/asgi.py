db6e10c750cc1c758cb296fcb8faf7150d42c883
codeAKstan <boystanismoses@gmail.com>
2024-12-07 15:45:40 +0100

    Setting up the project

---
+++ Swivly/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Swivly project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swivly.settings')
+
+application = get_asgi_application()
