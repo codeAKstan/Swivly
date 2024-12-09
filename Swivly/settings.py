f01b974aa67e61df111a0ef56245430de69da2ed
codeAKstan <boystanismoses@gmail.com>
2024-12-09 15:32:32 +0100

    changed db to postgresql

--- Swivly/settings.py
+++ Swivly/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'Swivly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'swivly_db',
+        'USER': 'postgres',
+        'PASSWORD': '554433',
+        'PORT': '5432',
+        'HOST': '127.0.0.1',
+        'PORT':'5432'
     }
 }
 
