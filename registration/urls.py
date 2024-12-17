e8cc5b5b2c3d54208b4bb6f0b7c4206305f213cc
codeAKstan <boystanismoses@gmail.com>
2024-12-17 10:00:09 +0100

    Log out working

--- registration/urls.py
+++ registration/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='registration:login'), name='logout'),
     path('', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name='profile')
 
