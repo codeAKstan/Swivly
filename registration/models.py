717f8b66471a242977ae319090c053f5c4c5ceae
codeAKstan <boystanismoses@gmail.com>
2024-12-23 10:09:10 +0100

    added the tel field

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -15,6 +16,7 @@ class Profile(models.Model):
         role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='buyer')
         photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
         address = models.CharField(max_length=200, null=True)
+        tel = PhoneNumberField(null=True, blank=True)
 
         def __str__(self):
             return f'{self.user.username} Profile'
