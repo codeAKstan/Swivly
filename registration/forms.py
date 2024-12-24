c6b3da69e7d93d4e6b4d66e80c4c4c436f160354
codeAKstan <boystanismoses@gmail.com>
2024-12-24 17:20:36 +0100

    Added the shop model for categories and products

--- registration/forms.py
+++ registration/forms.py
@@ -45,4 +45,4 @@ class UserEditForm(forms.ModelForm):
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['role','address', 'tel', 'photo']
+        fields = ['photo','address', 'tel', 'role']
