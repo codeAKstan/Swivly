04d481f481b5776c884b3cfe8bcb03f1ed152aaf
codeAKstan <boystanismoses@gmail.com>
2024-12-29 09:54:59 +0100

    Modified the address book in checkout page

--- cart/forms.py
+++ cart/forms.py
@@ -4,9 +4,11 @@ from registration.models import Profile
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['address', 'city', 'state']
+        fields = ['address', 'city', 'state', 'tel']
         widgets = {
             'address': forms.TextInput(attrs={'class': 'form-control'}),
             'city': forms.TextInput(attrs={'class': 'form-control'}),
             'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'tel': forms.TextInput(attrs={'class': 'form-control'}),
+
         }
