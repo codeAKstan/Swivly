f802ad4ead69ef35105526c7be34e0c41ba87d4e
codeAKstan <boystanismoses@gmail.com>
2025-01-10 05:24:37 +0100

    Added model for Services

--- registration/models.py
+++ registration/models.py
@@ -107,3 +107,28 @@ class ProductImage(models.Model):
         return f"Image for {self.product.name}"
 
 
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name="services")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
