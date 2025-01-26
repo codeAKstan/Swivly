6d2c663619a8dd88abd777fe7e236457f31bd5a1
codeAKstan <boystanismoses@gmail.com>
2025-01-26 06:41:31 +0100

    Modified the settings

--- Swivly/settings.py
+++ Swivly/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,11 +24,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-wcty9n=727ukz%0s0r-319r$lce%a&#eowg_qvzrlkpnr!(&n2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
 
 
 ALLOWED_HOSTS = ['*']
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'swivly_db',
+        'USER': 'postgres',
+        'PASSWORD': '554433',
+        'PORT': '5432',
+        'HOST': '127.0.0.1',
+        'PORT':'5432'
+    }
+}
 # Application definition
 
 INSTALLED_APPS = [
